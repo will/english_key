84f10253ef1e78387c9d7d2cbcf75b5bad149f90
Will Leinweber <will@bitfission.com>
2011-12-03 22:35:57 -0800

    uuid

--- lib/english_key.rb
+++ lib/english_key.rb
@@ -2,7 +2,16 @@ require 'securerandom'
 require_relative 'english_key/rfc1751'
 
 module EnglishKey
-  def self.uuid
-    SecureRandom.uuid
+  extend self
+  def uuid
+    encode(SecureRandom.random_number(2**128 - 1)).tr(' ', '_')
+  end
+
+  def encode(num)
+    RFC1751.encode num
+  end
+
+  def decode(str)
+    RFC1751.decode str
   end
 end
