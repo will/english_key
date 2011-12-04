00bae1674227df220e4878c8015cceb46fe21651
Will Leinweber <will@bitfission.com>
2011-12-03 21:24:47 -0800

    encode works

--- lib/english_key/rfc1751.rb
+++ lib/english_key/rfc1751.rb
@@ -1,6 +1,32 @@
 require_relative 'rfc1751_words'
 module EnglishKey
   module RFC1751
+    extend self
+    def encode(num)
+      parts = []
+      while num > 0
+        parts.unshift encode_64bit( num % 2**64 )
+        num /= 2**64
+      end
 
+      parts.join ' '
+    end
+
+    private
+
+    def encode_64bit(num)
+      bits = 63.downto(0).map{|i| num[i] }
+
+      keys = bits.each_slice(11).map {|bit_array| bit_array_to_int(bit_array) }
+
+      checksum = (0..63).step(2).inject(0) { |sum,j| sum + 2*num[j+1] + num[j] }
+      keys[5] = (keys[5] << 2) + (checksum & 3)
+
+      keys.map {|idx| WORDS[idx] }
+    end
+
+    def bit_array_to_int(arry)
+      arry.reverse.each.with_index.inject(0) {|sum, (n,pwr)| sum + n * 2**pwr }
+    end
   end
 end
